edb0156c6748fff070e9dde9e6d958e20790b09e
amanrawat16 <amanrawat9916@gmail.com>
2022-09-11 22:40:23 +0530

    install more packages and restructure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode:"jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",],
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily:{
